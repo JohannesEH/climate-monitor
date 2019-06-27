41c8e1ac1933fe8f012b42493229d4752a63c8d4
Johannes Hansen <johannesh1980@gmail.com>
2019-06-27 14:42:24 +0200

    Added base console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace climate_monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
