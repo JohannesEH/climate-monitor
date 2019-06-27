055b26b08f12270b2795336a05a3adbd0482571d
Johannes Hansen <johannesh1980@gmail.com>
2019-06-27 15:59:15 +0200

    Added rpio for testing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JohannesEH/climate-monitor
+
+go 1.12
+
+require github.com/stianeikeland/go-rpio/v4 v4.4.0
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
 import "fmt"
+import "github.com/stianeikeland/go-rpio/v4"
 
 func main() {
 	fmt.Println("Hello World")
+	rpio.Open()
+
+	// pin := rpio.Pin(10)
+
+	// pin.Output() // Output mode
+	// pin.High()   // Set pin High
+	// pin.Low()    // Set pin Low
+	// pin.Toggle() // Toggle pin (Low -> High -> Low)
+
+	// pin.Input()       // Input mode
+	// res := pin.Read() // Read state from pin (High / Low)
+
+	// pin.Mode(rpio.Output) // Alternative syntax
+	// pin.Write(rpio.High)  // Alternative syntax
+
+	// pin.PullUp()
+	// pin.PullDown()
+	// pin.PullOff()
+
+	// pin.Pull(rpio.PullUp)
+
+	rpio.Close()
+	fmt.Println("Bye World")
 }
